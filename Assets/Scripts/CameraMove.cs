4d866a5277d982d479d5bf7ec0b98914dc528f6c
FeshchenkoIvan <vaneskrutoy@mail.ru>
2022-08-07 19:25:24 +0300

    UpdateCameraMove

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,84 +5,86 @@ using UnityEngine.EventSystems;
 
 public class CameraMove : MonoBehaviour
 {
+    [SerializeField] private GameObject Camera;
+    [SerializeField] private float sensitivity= 0.1f;
     public bool CameraRotation;
-    public bool PhasePanelFigures;
-    public float X = 0;
-    public float Y = 0;
+    private Touch[] touches;
+    private bool PhasePanelFigures;
+    private float X = 0;
+    private float Y = 0;
+    private float distance;
 
-    float distanse;
-    //int SpeedRotation=10;
-    [SerializeField]
-    private GameObject camera;
     void Start()
     {
         CameraRotation = true;
         PhasePanelFigures = false;
     }
 
-    
     void Update()
     {
         if (CameraRotation)
         {
-            if (Input.touchCount>0)
-                if (Input.GetTouch(0).phase == TouchPhase.Began)
-                    if (Input.GetTouch(0).position.y < 320) PhasePanelFigures = true;
-
+            CheckClickPanelFigures();
             if (!PhasePanelFigures)
             {
-                Touch[] touches = Input.touches;
+                touches = Input.touches;
                 if (touches.Length == 1)
-                {
-                    Y += touches[0].deltaPosition.x * 0.1f;
-                    if (X - touches[0].deltaPosition.y * 0.1f < 90 && X - touches[0].deltaPosition.y * 0.1f > 0) //ограничение на угол
-                        X -= touches[0].deltaPosition.y * 0.1f;
-                }
-                transform.rotation = Quaternion.Euler(X, Y, 0);
-
+                    RotationCamera();
 
                 if (touches.Length == 2)
-                {
-                    float distanseNow = System.Math.Abs(Vector2.Distance(touches[0].position, touches[1].position));
-
-                    if (distanse == 0) { distanse = distanseNow; }
-                    else
-                    {
-                        if (distanseNow < distanse)
-                        {
-                            Vector3 NewPosition = camera.transform.localPosition;
-                            NewPosition.z -= (distanse - distanseNow) / 100;
-                            if (NewPosition.z > -20 && NewPosition.z < -5)
-                            {
-                                camera.transform.localPosition = NewPosition;
-                                distanse = distanseNow;
-                            }
-                        }
-                        else
-                        {
-                            Vector3 NewPosition = camera.transform.localPosition;
-                            NewPosition.z += (distanseNow - distanse) / 100;
-                            if (NewPosition.z > -20 && NewPosition.z < -5)
-                            {
-                                camera.transform.localPosition = NewPosition;
-                                distanse = distanseNow;
-                            }
-                        }
-                    }
-                }
-                if (touches.Length < 2) { distanse = 0; }
+                    ZoomingCamera();
+                else
+                    distance = 0;
             }
-
-            if (Input.touchCount > 0)
-                if (Input.GetTouch(0).phase == TouchPhase.Ended)
-                    PhasePanelFigures = false;
         }
     }
 
-
-    public void SwitchCameraRotationActive()
+    void CheckClickPanelFigures()
     {
-        CameraRotation = !CameraRotation;
+        if (Input.touchCount > 0)
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                if (Input.GetTouch(0).position.y < 320) PhasePanelFigures = true;
+            }
+            else 
+                if (Input.GetTouch(0).phase == TouchPhase.Ended) PhasePanelFigures = false;
     }
 
+    void RotationCamera()
+    {
+        Y += touches[0].deltaPosition.x * sensitivity;
+        if (X - touches[0].deltaPosition.y * sensitivity < 90 && X - touches[0].deltaPosition.y * sensitivity > 0)
+            X -= touches[0].deltaPosition.y * sensitivity;
+        transform.rotation = Quaternion.Euler(X, Y, 0);
+    }
+
+    void ZoomingCamera()
+    {
+        float distanceNow = System.Math.Abs(Vector2.Distance(touches[0].position, touches[1].position));
+
+        if (distance == 0) { distance = distanceNow; }
+        else
+        {
+            if (distanceNow < distance)
+            {
+                Vector3 NewPosition = Camera.transform.localPosition;
+                NewPosition.z -= (distance - distanceNow) / 100;
+                if (NewPosition.z > -20 && NewPosition.z < -5)
+                {
+                    Camera.transform.localPosition = NewPosition;
+                    distance = distanceNow;
+                }
+            }
+            else
+            {
+                Vector3 NewPosition = Camera.transform.localPosition;
+                NewPosition.z += (distanceNow - distance) / 100;
+                if (NewPosition.z > -20 && NewPosition.z < -5)
+                {
+                    Camera.transform.localPosition = NewPosition;
+                    distance = distanceNow;
+                }
+            }
+        }
+    }
 }
