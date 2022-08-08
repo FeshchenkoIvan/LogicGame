f44d2b9f88d23d4558684380773bf854203b58b2
FeshchenkoIvan <vaneskrutoy@mail.ru>
2022-08-08 15:45:31 +0300

    Update LevelCompleted.cs

--- Assets/Scripts/LevelCompleted.cs
+++ Assets/Scripts/LevelCompleted.cs
@@ -7,13 +7,36 @@ public class LevelCompleted : MonoBehaviour
 {
     [SerializeField] private CheckCollisionPoint[] checkCollisionPoints;
     private GameObject PanelCompleteLevel;
-    private bool levelCompleted;
     private int CountBoxCollisions;
     void Start()
     {
         PanelCompleteLevel = GameObject.Find("PanelCompleteLevel");
         CountBoxCollisions = checkCollisionPoints.Length;
-        levelCompleted = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+            CheckLevelComplete();
+    }
+    void CheckLevelComplete()
+    {
+        for (int i = 0; i < CountBoxCollisions; i++)
+        {
+            if (checkCollisionPoints[i].collision == false)
+                break;
+            else
+                if (i == CountBoxCollisions - 1)
+                    if (CountBoxCollisions == AllCubesInScene())
+                    {
+                        if (DataHolder._levelStart == PlayerPrefs.GetInt("Classic"))
+                        {
+                            PlayerPrefs.SetInt("Classic", DataHolder._levelStart + 1);
+                        }
+                        PanelCompleteLevel.transform.GetChild(0).gameObject.SetActive(true);
+                        transform.gameObject.SetActive(false);
+                    }
+        }
     }
     int AllCubesInScene()
     {
@@ -21,9 +44,9 @@ public class LevelCompleted : MonoBehaviour
         var figures = GameObject.FindGameObjectsWithTag("Figure");
         foreach (var figure in figures)
         {
-            if (figure.transform.position.y>0)
+            if (figure.transform.position.y > 0)
             {
-                if (figure.GetComponent<ActivateFigure>().indexFigure==1)
+                if (figure.GetComponent<ActivateFigure>().indexFigure == 1)
                     count += 3;
                 else
                     count += 4;
@@ -31,34 +54,4 @@ public class LevelCompleted : MonoBehaviour
         }
         return count;
     }
-    void Update()
-    {
-        if (levelCompleted)
-        {
-            if (DataHolder._levelStart == PlayerPrefs.GetInt("Classic"))
-            {
-                PlayerPrefs.SetInt("Classic", DataHolder._levelStart + 1);////[0] это первый уровень в PlayerPrefs
-            }
-            PanelCompleteLevel.transform.GetChild(0).gameObject.SetActive(true);
-            //transform.root.gameObject.SetActive(false);
-            transform.gameObject.SetActive(false);
-        }
-
-        if (Input.GetMouseButtonUp(0))//(Input.GetTouch(0).phase == TouchPhase.Ended)
-        {
-            //levelCompleted = false;
-            for (int i = 0; i < CountBoxCollisions; i++)
-            {
-                if (checkCollisionPoints[i].collision == false)
-                    break;
-                else
-                    if (i== CountBoxCollisions - 1)
-                        if (CountBoxCollisions == AllCubesInScene())
-                            levelCompleted = true;
-            }
-            Debug.Log("levelCompleted=" + levelCompleted + ", AllCubes=" + AllCubesInScene());
-        }
-    }
-
-    
 }
